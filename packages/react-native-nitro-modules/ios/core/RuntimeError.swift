30f0ed4eca0966d8946149fb14f981a45fdc64de
Marc Rousavy <me@mrousavy.com>
2026-09-22 14:13:36 +0200

    fix: Preserve RuntimeError messages in localizedDescription

--- packages/react-native-nitro-modules/ios/core/RuntimeError.swift
+++ packages/react-native-nitro-modules/ios/core/RuntimeError.swift
@@ -5,17 +5,23 @@
 //  Created by Marc Rousavy on 17.07.24.
 //
 
+import Foundation
+
 /// Represents an error that occured at any point during the application's runtime.
 ///
 /// Throw this error in Nitro Modules to provide clear and concise error messages to JS.
 @frozen
-public enum RuntimeError: Error, CustomStringConvertible {
+public enum RuntimeError: LocalizedError, CustomStringConvertible {
   case error(withMessage: String)
 
   public var description: String {
     switch self {
     case .error(let message): return message
     }
+  }
+
+  public var errorDescription: String? {
+    return description
   }
 
   /**
